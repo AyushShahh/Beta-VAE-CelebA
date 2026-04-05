f67df5b864da0f935d026f3d87506a44bad80484
Ayush Shah <shahayush001@gmail.com>
2026-04-05 14:42:58 +0530

    Update download file

--- download.py
+++ download.py
@@ -1,9 +1,17 @@
 from huggingface_hub import snapshot_download
+import os
+import shutil
 
 snapshot_download(
     repo_id="ayushshah/beta-vae-capacity-annealing-celeba",
     repo_type="model",
-    local_dir=".",
-    ignore_patterns=["README.md", ".gitignore"],
+    local_dir="./huggingface",
     local_dir_use_symlinks=False
 )
+
+os.makedirs("checkpoints", exist_ok=True)
+
+shutil.move("./huggingface/directions", "./")
+shutil.move("./huggingface/model.safetensors", "./checkpoints/model.safetensors")
+
+shutil.rmtree("./huggingface")
